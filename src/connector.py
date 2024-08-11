0fe36ed8f0b17e73291a638affd733529ff6540f
KirSouL <Sokil-Ki-So@yandex.ru>
2024-08-11 22:00:45 +0300

    update file connector.py in directory src

--- src/connector.py
+++ src/connector.py
@@ -1,5 +1,42 @@
 import psycopg2
-import json
+from configparser import ConfigParser as ConfPars
+
+
+class ConnectorPSQL:
+    connection = None
+    cursor = None
+
+    def __init__(self) -> None:
+        parser = ConfPars()
+        parser.read("database.ini")
+
+        database = {}
+        params = parser.items("postgresql")
+
+        for param in params:
+            database[param[0]] = param[1]
+
+        connect = psycopg2.connect(**database)
+        ConnectorPSQL.connection = connect
+        ConnectorPSQL.cursor = connect.cursor()
+
+    def request_action(self, string: str, params: dict):
+        result = None
+
+        if params:
+            ConnectorPSQL.cursor.execute(string, params)
+        else:
+            ConnectorPSQL.cursor.execute(params)
+
+        if ConnectorPSQL.cursor.description is not None:
+            result = ConnectorPSQL.cursor.fetchone()
+
+        ConnectorPSQL.connection.commit()
+
+        return result
+
+
+
 
 
 def connect_to_psql(user_host: str, user_port: str, user_database: str, user_name: str,
@@ -7,11 +44,11 @@ def connect_to_psql(user_host: str, user_port: str, user_database: str, user_nam
     connect = psycopg2.connect(
         host=f"{user_host}",
         port=f"{user_port}",
-        database=f"{user_database}",
         user=f"{user_name}",
         password=f"{user_password}"
     )
     curs = connect.cursor()
+    curs.execute("CREATE DATABASE employers and vacancies;")
     curs.execute("DROP TABLE employers;")
     curs.execute("CREATE TABLE employers (employer_id CHAR(5) PRIMARY KEY, company_name VARCHAR(100) NOT NULL, contact_name varchar(100) NOT NULL);")
     connect.commit()
@@ -24,24 +61,21 @@ def connect_to_psql(user_host: str, user_port: str, user_database: str, user_nam
     return cur_cust
 
 
-print(connect_to_psql("localhost", "5432", "vac_employers",
-                "postgres", "1004"))
+
 
 
 def cont(user_host: str, user_port: str, user_database: str, user_name: str, user_password: str) -> None:
     connect = psycopg2.connect(
         host=f"{user_host}",
         port=f"{user_port}",
-        database=f"{user_database}",
         user=f"{user_name}",
         password=f"{user_password}"
     )
     curs = connect.cursor()
-    curs.execute("DROP TABLE customers;")
+    curs.execute("DROP TABLE employers;")
     connect.commit()
     curs.close()
     connect.close()
 
 
-print(cont("localhost", "5432", "vac_employers",
-                "postgres", "1004"))
+
